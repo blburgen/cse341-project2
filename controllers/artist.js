083a6cbd4ff8a5a0959005aac702b66c8e136033
Brady Burgener <bradylbsee@gmail.com>
2026-05-30 15:09:00 -0600

    update key

--- controllers/artist.js
+++ controllers/artist.js
@@ -1,8 +1,7 @@
 const db = require('../models');
 const Artist = db.artists;
 
-const apiKey =
-  'Ezl0961tEpx2UxTZ5v2uKFK91qdNAr5npRlMT1zLcE3Mg68Xwaj3N8Dyp1R8IvFenrVwHRllOUxF0Og00l0m9NcaYMtH6Bpgdv7N';
+const apiKey = process.env.apiKey;
 
 exports.create = (req, res) => {
   // Validate request
