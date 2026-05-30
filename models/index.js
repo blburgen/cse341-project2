d9286dcc640a9ce4f396c3f6772c28cdb5d195dd
Brady Burgener <bradylbsee@gmail.com>
2026-05-30 14:34:27 -0600

    second

--- models/index.js
+++ models/index.js
@@ -7,5 +7,6 @@ const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
 db.pictures = require('./pictures.js')(mongoose);
+db.artists = require('./artists.js')(mongoose);
 
 module.exports = db;
